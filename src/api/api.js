47f09d22141e88d55c8168934d2692d70bffdf7c
Route66Auto <ti.route66auto@gmail.com>
2025-01-14 12:56:28 -0300

    barras removidas

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,19 @@
 import axios from "axios";
-const local = "http://localhost:5000";
-const production = "https://route66auto-api.onrender.com/";
 
-let api_url = ''
-let mode = 'pro'
+const local = "http://localhost:5000";
+const production = "https://route66auto-api.onrender.com";  // Mantém a barra no final
+
+let api_url = '';
+let mode = 'pro';
 
 if (mode === 'pro') {
-  api_url = production
+  api_url = production;
 } else {
-  api_url = local
+  api_url = local;
 }
+
 const api = axios.create({
-  baseURL: `${api_url}/api`,
+  baseURL: `${api_url.replace(/\/$/, '')}/api`,  // Remove a barra final se houver antes de adicionar '/api'
   withCredentials: true
 });
 
